980906a9797d76e65d118c074167985e22f2b996
AhmadSameh <ahmadsameh660@gmail.com>
2025-08-14 14:37:41 +0300

    finish all message related functions

--- internal/message/message.go
+++ internal/message/message.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 )
 
@@ -22,6 +23,20 @@ type Message struct {
 	Payload []byte
 }
 
+func FormatRequest(index, begin, length int) *Message {
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], uint32(index))
+	binary.BigEndian.PutUint32(payload[4:8], uint32(begin))
+	binary.BigEndian.PutUint32(payload[8:12], uint32(length))
+	return &Message{ID: MsgRequest, Payload: payload}
+}
+
+func FormatHave(index int) *Message {
+	payload := make([]byte, 4)
+	binary.BigEndian.PutUint32(payload, uint32(index))
+	return &Message{ID: MsgHave, Payload: payload}
+}
+
 func (m Message) Serialize() []byte {
 	length := uint32(1 + len(m.Payload))
 	buf := make([]byte, 4+length)
@@ -29,6 +44,32 @@ func (m Message) Serialize() []byte {
 	buf[4] = byte(m.ID)
 	copy(buf[5:], m.Payload)
 	return buf
+}
+
+func (m Message) ParsHave(msg *Message) (int, error) {
+	if len(msg.Payload) != 4 {
+		return 0, fmt.Errorf("expected payload length 4, got length %d", len(msg.Payload))
+	}
+	index := int(binary.BigEndian.Uint32(msg.Payload))
+	return index, nil
+}
+
+func (m Message) ParsePiece(index int, buf []byte, msg *Message) (int, error) {
+
+	pieceIndex := int(binary.BigEndian.Uint32(msg.Payload[0:4]))
+	if pieceIndex != index {
+		return 0, fmt.Errorf("expected index %d, got %d", index, pieceIndex)
+	}
+	pieceOffset := int(binary.BigEndian.Uint32(msg.Payload[4:8]))
+	if pieceOffset >= len(buf) {
+		return 0, fmt.Errorf("begin offset too high. %d >= %d", pieceOffset, len(buf))
+	}
+	data := msg.Payload[8:]
+	if pieceOffset+len(data) > len(buf) {
+		return 0, fmt.Errorf("data too long [%d] for offset %d with length %d", len(data), pieceOffset, len(buf))
+	}
+	copy(buf[pieceOffset:], data)
+	return len(data), nil
 }
 
 func Read(r io.Reader) (*Message, error) {
