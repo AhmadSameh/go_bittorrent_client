3a111781dab07591aa223e426a349f1b638deea2
ahmad.sameh <ahmad.sameh@ittia.com>
2025-08-15 22:29:51 +0300

    add bitfield test

--- internal/bitfield/bitfield.go
+++ internal/bitfield/bitfield.go
@@ -5,11 +5,17 @@ type BitField []byte
 func (bf BitField) HasPiece(index int) bool {
 	byteIndex := index / 8
 	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
 	return bf[byteIndex]>>(7-offset)&1 != 0
 }
 
 func (bf BitField) SetPiece(index int) {
 	byteIndex := index / 8
 	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
 	bf[byteIndex] |= 1 << (7 - offset)
 }
